a19207972174eed3957d690d51de5e27885ff477
lihaibin150 <210190765@qq.com>
2019-12-11 15:10:15 +0800

    fz

--- app/src/main/java/com/wd/doctor/MainActivity.java
+++ app/src/main/java/com/wd/doctor/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.bwei.example.mylibrary.Test.ToastUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToastUtils.show("分支");
     }
 }
